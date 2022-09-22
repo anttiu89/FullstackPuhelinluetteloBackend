800b97488fb6514b2a484c9a38360f4c7caf0146
Antti Uusimäki <antti.uusimaki@etteplan.com>
2022-09-22 22:34:52 +0300

    3.8*: puhelinluettelon backend step8

--- index.js
+++ index.js
@@ -3,7 +3,13 @@ const app = express()
 const morgan = require("morgan")
 
 app.use(express.json())
-app.use(morgan("tiny"))
+
+morgan.token("myToken", (request, response) => { 
+    if (request.body) {
+        return JSON.stringify(request.body)
+    }
+})
+app.use(morgan(":method :url :status :res[content-length] - :response-time ms :myToken"))
 
 const getRandomInt = (min, max) => {
     min = Math.ceil(min)
