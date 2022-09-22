1ad44d89c7fe87cf0db767d302a469c05ea71631
Antti Uusimäki <antti.uusimaki@etteplan.com>
2022-09-22 20:54:54 +0300

    3.3: puhelinluettelon backend step3

--- index.js
+++ index.js
@@ -38,6 +38,19 @@ app.get('/api/persons', (req, res) => {
     res.json(phonebook)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = phonebook.find(person => {
+        return person.id === id
+    })
+    console.log(person)
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
